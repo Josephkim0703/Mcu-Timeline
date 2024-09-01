a909c50698a02b10221aff32c42d59624db35a3a
Joseph <josephkim0703@gmail.com>
2024-08-31 19:44:58 -0700

    commit

--- marvel/src/util/Card.jsx
+++ marvel/src/util/Card.jsx
@@ -2,19 +2,17 @@ import React, { useState, useEffect } from "react";
 import "../css/card.css";
 import { marvel } from "./data.js";
 function Card() {
-  const [card, setCard] = useState("");
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    const allImages = marvel.map((character) => character.image);
-    setCard(allImages);
+    const x = marvel.map((char) => char.image);
+    setCards(x);
   }, []);
 
   return (
     <>
       <div id="card">
-      {card.map((image, index) => (
-                    <img key={index} src={image} />
-                ))}
+      {cards.map((image, index) => (<img key={index} src={image} />))}
       </div>
     </>
   );
--- marvel/src/util/Header.jsx
+++ marvel/src/util/Header.jsx
@@ -1,12 +1,81 @@
 import "../css/index.css";
+import minute from "../assets/char/miss_minute.png";
+import { useState, useEffect, useRef } from "react";
+function Header() {
+  const [hide, setHide] = useState(false);
+  const [complete, setComplete] = useState(false);
+  const slide = "slide";
+  const slideOut = "slideOut";
+  const buttonRef = useRef(null);
 
-function Header(){
+  function handleClick(e) {
+    if (complete) {
+      setComplete(false);
+      buttonRef.current.classList.add(slideOut);
 
-    return(
-       <header>
-        <h1>MCU Timeline</h1>
-       </header>
-    )
+      setTimeout(() => {
+        buttonRef.current.classList.remove(slide);
+
+        setTimeout(() => {
+          setHide(false);
+        }, 300);
+      }, 0);
+    } else {
+      setHide(true);
+      setComplete(true);
+      buttonRef.current.classList.add(slide);
+
+      setTimeout(() => {
+        buttonRef.current.classList.remove(slideOut);
+      }, 0);
+    }
+  }
+
+  useEffect(() => {
+    function handleClickOut(e) {
+      if (!e.target.closest("#main_button")) {
+        setComplete(false);
+        buttonRef.current.classList.add(slideOut);
+
+        setTimeout(() => {
+          setTimeout(() => {
+            setHide(false);
+          }, 300);
+          buttonRef.current.classList.remove(slide);
+        }, 0);
+      }
+    }
+
+    document.addEventListener("click", handleClickOut);
+
+    return () => {
+      document.removeEventListener("click", handleClickOut);
+    };
+  }, []);
+
+  return (
+    <header>
+      <h1>MCU Timeline</h1>
+      <div id="slider" ref={buttonRef}>
+        <button type="button" id="main_button">
+          <img src={minute} alt="" onClick={handleClick} />
+        </button>
+        {hide && (
+          <div id="option">
+            <button type="button" className="option">
+              Movies
+            </button>
+            <button type="button" className="option">
+              TV-Shows
+            </button>
+            <button type="button" className="option">
+              All
+            </button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
