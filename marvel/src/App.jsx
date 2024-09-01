69b64a2683ddfd8d03d39c7e6da59fae394091da
Joseph <josephkim0703@gmail.com>
2024-09-01 00:23:02 -0700

    data

--- marvel/src/App.jsx
+++ marvel/src/App.jsx
@@ -9,24 +9,42 @@ function App() {
   const [background, setBackground] = useState(galaxy);
 
   const [cards, setCards] = useState([]);
+  const [cards2, setCards2] = useState([]);
 
   function tvButton() {
     const x = marvel
       .filter((element) => element.type === "show")
       .map((element) => element.image);
     setCards(x);
+
+    const top = x.filter((_, index) => index % 2 === 0);
+    const bottom = x.filter((_, index) => index % 2 === 1);
+    setCards(top);
+    setCards2(bottom);
   }
 
   function movieButton() {
     const x = marvel
       .filter((element) => element.type === "movie")
       .map((element) => element.image);
-    setCards(x);
+
+    const top = x.filter((_, index) => index % 2 === 0);
+    const bottom = x.filter((_, index) => index % 2 === 1);
+    setCards(top);
+    setCards2(bottom);
   }
 
   function allButton() {
-    const x = marvel.map((char) => char.image);
+    const x = marvel
+      .filter((_, index) => index % 2 === 0)
+      .map((char) => char.image);
+
+    const y = marvel
+      .filter((_, index) => index % 2 === 1)
+      .map((char) => char.image);
+
     setCards(x);
+    setCards2(y);
   }
 
   useEffect(() => {
@@ -35,9 +53,14 @@ function App() {
 
   return (
     <>
-      <Header buttonShow={tvButton} buttonMovie={movieButton} buttonAll={allButton}/>
+      <Header
+        buttonShow={tvButton}
+        buttonMovie={movieButton}
+        buttonAll={allButton}
+      />
       <div id="timeline">
-        <Card cards={cards} />
+        <Card cards={cards} id="top_list"/>
+        <Card cards={cards2} id="bottom_list"/>
       </div>
       <img src={background} alt="" id="background" />
     </>
--- marvel/src/util/data.js
+++ marvel/src/util/data.js
@@ -79,6 +79,562 @@ import echo from '../assets/cards/echo.jpg';
 import deadpool from '../assets/cards/deadpool.jpg';
 
 const marvel = [
+    {
+        name: "Captain America: The First Avenger",
+        date: "July 2011",
+        image: CaptainAmerica,
+        type: "movie",
+        time: "1943-1945"
+    },
+    {
+        name: "Agent Carter",
+        date: "January 2015 – February 2015",
+        image: carter,
+        type: "show",
+        time: "1946"
+    },
+    {
+        name: "Agent Carter season 2 ",
+        date: "January 2016 – February 2016",
+        image: carter2,
+        type: "show",
+    },
+    {
+        name: "Captain Marvel",
+        date: "March 2019",
+        image: CaptainMarvel,
+        type: "movie",
+        time: "1995"
+    },
+    {
+        name: "Iron Man",
+        date: "May 2008",
+        image: ironMan,
+        type: "movie",
+        time: "2008"
+    },
+    {
+        name: "Iron Man 2",
+        date: "April 2010",
+        image: ironMan2,
+        type: "movie",
+        time: "2010"
+    }, 
+    {
+        name: "Thor",
+        date: "April 2011",
+        image: thor,
+        type: "movie",
+        time: "2010"
+    },
+    {
+        name: "The Incredible Hulk",
+        date: "June 2008",
+        image: hulk,
+        type: "movie",
+        time: "2010"
+    },
+    {
+        name: "The Avengers",
+        date: "April 2012",
+        image: avengers,
+        type: "movie",
+        time: "2012"
+    }
+    ,{
+        name: "Iron Man 3",
+        date: "April 2013",
+        image: ironMan3,
+        type: "movie",
+        time: "2013"
+    },
+    {
+        name: "Agents of SHIELD",
+        date: "September 2013 – May 2014",
+        image: shield,
+        type: "show",
+        time: "2013"
+    },
+    {
+        name: "Thor: The Dark World",
+        date: "November 2013",
+        image: thor2,
+        type: "movie",
+        time: "2013"
+    },
+    {
+        name: "Captain America: The Winter Soldier",
+        date: "March 2014",
+        image: CaptainAmerica2,
+        type: "movie",
+        time: "2014"
+    },
+    {
+        name: "Guardians of the Galaxy",
+        date: "July 2014",
+        image: gotg,
+        type: "movie",
+        time: "2014"
+    },
+    {
+        name: "Guardians of the Galaxy Vol 2",
+        date: "April 2017",
+        image: gotg2,
+        type: "movie",
+        time: "2014"
+    },
+    {
+        name: "I Am Groot",
+        date: "August 2022",
+        image: groot,
+        type: "show",
+        time: "2014"
+    },
+    {
+        name: "I Am Groot season 2",
+        date: "September 2023",
+        image: groot2,
+        type: "show",
+        time: "2014"
+    },
+    {
+        name: "Daredevil",
+        date: "April 2015",
+        image: dareDevil,
+        type: "show",
+        time: "2014"
+    },
+    {
+        name: "Jessica Jones",
+        date: "November 2015",
+        image: jones,
+        type: "show",
+        time: "2014"
+    },
+    {
+        name: "Agents of SHIELD season 2",
+        date: "September 2014 – May 2015",
+        image: shield2,
+        type: "show",
+        time: "2015"
+    },
+    {
+        name: "Avengers: Age of Ultron",
+        date: "April 2015",
+        image: avengers2,
+        type: "movie",
+        time: "2015"
+    },
+    {
+        name: "Ant-Man",
+        date: "July 2015",
+        image: antman,
+        type: "movie",
+        time: "2015"
+    },
+    {
+        name: "Daredevil season 2",
+        date: "March 2016",
+        image: dareDevil2,
+        type: "show",
+        time: "2015"
+    },
+    {
+        name: "Luke Cage",
+        date: "September 2016",
+        image: cage,
+        type: "show",
+        time: "2015"
+    },
+    {
+        name: "Iron Fist",
+        date: "March 2017",
+        image: iron,
+        type: "show",
+        time: "2016"
+    },
+    {
+        name: "The Defenders",
+        date: "August 2017",
+        image: defenders,
+        type: "show",
+        time: "2016"
+    },
+    {
+        name: "Agents of SHIELD season 3",
+        date: "September 2015 – May 2016",
+        image: shield3,
+        type: "show",
+        time: "2016"
+    },
+    {
+        name: "Captain America: Civil War ",
+        date: "April 2016",
+        image: CaptainAmerica3,
+        type: "movie",
+        time: "2016"
+    },
+    {
+        name: "Black Widow",
+        date: "July 2021",
+        image: blackWidow,
+        type: "movie",
+        time: "2016"
+    },
+    ,{
+        name: "Black Panther",
+        date: "February 2018",
+        image: blackPanther,
+        type: "movie",
+        time: "2016"
+    },
+    {
+        name: "Inhumans",
+        date: "September 2017 – November 2017",
+        image: inhuman,
+        type: "show",
+        time: "2016"
+    },
+    {
+        name: "Spider-Man: Homecoming",
+        date: "July 2017",
+        image: spiderman,
+        type: "movie",
+        time: "2016"
+    },
+    {
+        name: "The Punisher",
+        date: "November 2017",
+        image: punisher,
+        type: "show",
+        time: "2016"
+    },
+    {
+        name: "Doctor Strange",
+        date: "October 2016",
+        image: doctorStrange,
+        type: "movie",
+        time: "2016-2017"
+    },
+    {
+        name: "Cloak & Dagger",
+        date: "June 2018 – August 2018",
+        image: cad,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Agents of SHIELD season 4",
+        date: "September 2016 – May 2017",
+        image: shield4,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Jessica Jones season 2",
+        date: "March 2018",
+        image: jones2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Luke Cage season 2",
+        date: "June 2018",
+        image: cage2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Iron Fist season 2",
+        date: "September 2018",
+        image: iron2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Daredevil season 3",
+        date: "October 2018",
+        image: dareDevil3,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Cloak & Dagger season 2",
+        date: "April 2019 – May 2019",
+        image: cad2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Thor: Ragnarok",
+        date: "October 2017",
+        image: thor3,
+        type: "movie",
+        time: "2017"
+    },
+    {
+        name: "Agents of SHIELD season 5",
+        date: "December 2017 – May 2018",
+        image: shield5,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Runaways",
+        date: "November 2017 – January 2018",
+        image: runaways,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Runaways season 2",
+        date: "December 2018",
+        image: runaways2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Runaways season 3",
+        date: "December 2019",
+        image: runaways3,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "The Punisher season 2",
+        date: "January 2019",
+        image: punisher2,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Helstrom",
+        date: "October 2020",
+        image: helstrom,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Jessica Jones season 3",
+        date: "June 2019",
+        image: jones3,
+        type: "show",
+        time: "2017"
+    },
+    {
+        name: "Ant-Man and the Wasp",
+        date: "July 2018",
+        image: antman2,
+        type: "movie",
+        time: "2018"
+    },
+    {
+        name: "Avengers: Infinity War",
+        date: "April 2018",
+        image: avengers3,
+        type: "movie",
+        time: "2018"
+    },
+    {
+        name: "Agents of SHIELD season 6",
+        date: "May 2019 – August 2019",
+        image: shield6,
+        type: "show",
+        time: "2018"
+    },
+    {
+        name: "Agents of SHIELD season 7",
+        date: "May 2020 – August 2020",
+        image: shield7,
+        type: "show",
+        time: "2018"
+    },
+    {
+        name: "Avengers: Endgame",
+        date: "April 2019",
+        image: avengers4,
+        type: "movie",
+        time: "2018-2023"
+    },
+    {
+        name: "Loki",
+        date: "June 2021 – July 2021",
+        image: loki,
+        type: "show",
+        time: "Time?"
+    },
+    {
+        name: "Loki season 2",
+        date: "October 2023 – November 2023",
+        image: loki2,
+        type: "show",
+        time: "Time?"
+    },
+    {
+        name: "What If...?",
+        date: "August 2021 – October 2021",
+        image: whatif,
+        type: "show",
+        time: "Multiverse"
+    },
+    {
+        name: " What If...? season 2",
+        date: "December 2023",
+        image: whatif2,
+        type: "show",
+        time: "Multiverse"
+    },
+    {
+        name: "WandaVision",
+        date: "January 2021 – March 2021",
+        image: wandavision,
+        type: "show",
+        time: "2023"
+    },
+    ,{
+        name: "Eternals",
+        date: "November 2021",
+        image: eternals,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "Shang-Chi and the Legend of the Ten Rings",
+        date: "September 2021",
+        image: shangchi,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "The Falcon and the Winter Soldier",
+        date: "March 2021 – April 2021",
+        image: falcon,
+        type: "show",
+        time: "2024"
+    },
+    {
+        name: "Deadpool & Wolverine",
+        date: "July 2024",
+        image: deadpool,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "Spider-Man: Far From Home",
+        date: "July 2019",
+        image: spiderman2,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "Spider-Man: No Way Home",
+        date: "December 2021",
+        image: spiderman3,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "Doctor Strange in the Multiverse of Madness",
+        date: "May 2022",
+        image: doctorStrange2,
+        type: "movie",
+        time: "2024"
+    },
+    {
+        name: "Hawkeye",
+        date: "November 2021 – December 2021",
+        image: hawkeye,
+        type: "show",
+        time: "2024"
+    },
+    {
+        name: "Moon Knight",
+        date: "March 2022 – May 2022",
+        image: moonknight,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "She-Hulk: Attorney at Law",
+        date: "August 2022 – October 2022",
+        image: shehulk,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "Ms Marvel",
+        date: "June 2022 – July 2022",
+        image: msmarvel,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "Thor: Love and Thunder",
+        date: "July 2022",
+        image: thor4,
+        type: "movie",
+        time: "2025"
+    },
+    {
+        name: "Secret Invasion",
+        date: "June 2023 – July 2023",
+        image: secret,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "Black Panther: Wakanda Forever",
+        date: "November 2022",
+        image: blackPanther2,
+        type: "movie",
+        time: "2025"
+    },
+    {
+        name: "The Marvels",
+        date: "November 2023",
+        image: CaptainMarvel2,
+        type: "movie",
+        time: "2025"
+    },
+    {
+        name: "Echo",
+        date: "January 2024",
+        image: echo,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "Werewolf By Night",
+        date: "October 2022",
+        image: werewolf,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "The Guardians of the Galaxy Holiday Special",
+        date: "November 2022",
+        image: gotg_special,
+        type: "show",
+        time: "2025"
+    },
+    {
+        name: "Guardians of the Galaxy Vol 3",
+        date: "May 2023",
+        image: gotg3,
+        type: "movie",
+        time: "2026"
+    },
+    {
+        name: "Ant-Man and The Wasp: Quantumania",
+        date: "February 2023",
+        image: antman3,
+        type: "movie",
+        time: "2026"
+    },
+    
+    ];
+
+const marvel_ReleaseDate = [
 {
     name: "Iron Man",
     date: "May 2008",
@@ -101,12 +657,6 @@ const marvel = [
     name: "Thor",
     date: "April 2011",
     image: thor,
-    type: "movie",
-},
-{
-    name: "Captain America: The First Avenger",
-    date: "July 2011",
-    image: CaptainAmerica,
     type: "movie",
 },
 {
@@ -554,5 +1104,6 @@ const marvel = [
     type: "movie",
 },
 ];
+
 
 export {marvel};
